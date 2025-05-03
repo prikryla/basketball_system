1a320e0bb04209fb9acd25489349bf265e4d8573
Adam Prikryl <adam.prikryl7@icloud.com>
2025-05-03 23:35:38 +0200

    Added EasyAdmin bundle

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController {
     
-    #[Route('homepage')]
+    #[Route('/home', name: 'home')]
     public function homepage(): Response{
-        return $this->render('homepage.html.twig');
+        return $this->render('base.html.twig');
     }
 }
