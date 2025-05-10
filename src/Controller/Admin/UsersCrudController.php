c948fd966b133006d2c32b645abc9cc0e894bc79
Adam Prikryl <adam.prikryl7@icloud.com>
2025-05-10 18:52:21 +0200

    Created a new Entity of the cities

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -38,17 +38,17 @@ class UsersCrudController extends AbstractCrudController
             TextField::new('last_name'),
             DateField::new('date_of_birth'),
             TextField::new('phone_number'),
-            ChoiceField::new('roles')
-            ->setChoices([
-                'Admin' => 'ROLE_ADMIN',
-                'User' => 'ROLE_USER',
-            ])
-            ->allowMultipleChoices()
-            ->renderExpanded(),
             TextField::new('email'),
             TextField::new('password')
                 ->setRequired(false)  // Make password field optional when editing
                 ->setFormTypeOption('attr', ['autocomplete' => 'new-password']),
+            ChoiceField::new('roles')
+                ->setChoices([
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER',
+                ])
+                ->allowMultipleChoices()
+                ->renderExpanded(),
         ];
     }
 
---
+++ src/Entity/Cities.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CitiesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CitiesRepository::class)]
+class Cities
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $distance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDistance(): ?int
+    {
+        return $this->distance;
+    }
+
+    public function setDistance(int $distance): static
+    {
+        $this->distance = $distance;
+
+        return $this;
+    }
+}
